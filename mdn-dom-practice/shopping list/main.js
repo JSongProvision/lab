c84fd97193a8bd80dcb79f9689416fb25fc5b2a1
JSongProvision <childrenofthefenceunite@gmail.com>
2025-05-28 13:03:18 -0400

    Add shopping list project and move dinosaur files
    Added a directory for shopping list exercise and reorganized the files
    for the other active learning on the "dinosau" page

---
+++ mdn-dom-practice/shopping list/main.js
@@ -0,0 +1,5 @@
+//MDN Active learning DOM Manipulation - Shopping List exercise
+
+const list = document.querySelector ("ul");
+const input = document.querySelector ("input");
+const button = document.querySelector ("button");
