80ca93830769376b96a5cacf0c4efcc15d1d26d4
JSongProvision <childrenofthefenceunite@gmail.com>
2025-05-28 19:47:49 -0400

    Finish shopping list activity
    Finished the shopping list activity for dom manip lesson

--- mdn-dom-practice/shopping list/main.js
+++ mdn-dom-practice/shopping list/main.js
@@ -3,3 +3,27 @@
 const list = document.querySelector ("ul");
 const input = document.querySelector ("input");
 const button = document.querySelector ("button");
+
+button.addEventListener("click", () => {
+    const myItem = input.value;
+    input.value = '';
+
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newButton = document.createElement("button");
+
+    newLi.appendChild(newSpan);
+    newLi.appendChild(newButton);
+
+    newSpan.textContent = myItem;
+    newButton.textContent = "Delete";
+
+    list.appendChild(newLi);
+
+    newButton.addEventListener("click", () => {
+        list.removeChild(newLi);
+    });
+
+    input.focus();
+});
+
